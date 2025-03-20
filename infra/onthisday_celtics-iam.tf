0efe12e1fbddd2d9ce9a5bb391756e4a007449a6
meagle21 <emanuel.eagle@gmail.com>
2025-03-20 12:43:45 -0400

    update

---
+++ infra/onthisday_celtics-iam.tf
@@ -0,0 +1,31 @@
+data "aws_iam_policy_document" "onthisday_celtics-assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "onthisday_celtics-iam_role" {
+  name  = "onthisday_celtics-ecr-permissions"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "ecr_policy_attachment" {
+  role       = aws_iam_role.onthisday_celtics-iam_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPullOnly"
+}
+
+# Permission for EventBridge to invoke Lambda
+resource "aws_lambda_permission" "allow_eventbridge" {
+  statement_id  = "AllowExecutionFromEventBridge"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.onthisday_celtics-lambda.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.onthisday_celtics-lambda_trigger.arn
+}
